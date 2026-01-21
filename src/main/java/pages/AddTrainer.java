d947ac0a69793a44ecbbebddfe036aaf3a501e8d
Subrata Ghosh <subrataghosh8228@gmail.com>
2026-01-21 12:36:42 +0530

    Addtrainer

--- src/main/java/pages/AddTrainer.java
+++ src/main/java/pages/AddTrainer.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -23,45 +24,52 @@ public class AddTrainer {
     public void clickOnTrainerManagement() {
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement trainerbutton= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(prop.getProperty("trainermanagement.menu.xpath"))));
+        WebElement trainerbutton= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(prop.getProperty("addtrainer.trainermanagement.xpath"))));
         trainerbutton.click();
 
     }
 
     public void clickOnAddTrainer() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement  trainer= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(prop.getProperty("addtrainer.menu.xpath"))));
+        WebElement  trainer= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(prop.getProperty("addtrainer.trainer.xpath"))));
         trainer.click();
     }
 
-    public void enterName() {
+    public void enterName(String trainerfullname) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement name= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(prop.getProperty("addtrainer.name.xpath"))));
-        name.sendKeys(prop.getProperty("addtrainer.name"));
+        name.sendKeys(trainerfullname);
     }
 
-    public void enterEmail() {
+    public void enterEmail(String traineremail) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement email= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(prop.getProperty("addtrainer.email.xpath"))));
-        email.sendKeys(prop.getProperty("addtrainer.email"));
+        email.sendKeys(traineremail);
     }
 
-    public void enterPhoneNumber() {
+    public void enterPhoneNumber(String trainerphonenumber) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement phone=wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(prop.getProperty("addtrainer.phonenumber.xpath"))));
-        phone.sendKeys(prop.getProperty("addtrainer.phonenumber"));
+        phone.sendKeys(trainerphonenumber);
     }
 
-    public void enterPassword() {
+    public void enterPassword(String trainerpassword) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement password =wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(prop.getProperty("addtrainer.password.xpath"))));
-        password.sendKeys(prop.getProperty("addtrainer.password"));
+        password.sendKeys(trainerpassword);
     }
 
     public void clickOnAddButton() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement clickonadd= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(prop.getProperty("addtrainer.addbutton.xpath"))));
         clickonadd.click();
+    }
+
+    public void deleteTrainer() {
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement delete = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(prop.getProperty("addtrainer.delete.xpath"))));
+        delete.click();
     }
 
     public void handleConfirmationPopup() {
--- src/test/java/AddTrainerTest.java
+++ src/test/java/AddTrainerTest.java
@@ -1,2 +1,42 @@
-public class AddTrainerTest {
+import base.BaseTest;
+import org.testng.annotations.Test;
+import pages.AddTrainer;
+import pages.LoginPage;
+
+import static config.ConfigReader.prop;
+
+public class AddTrainerTest extends BaseTest {
+
+    @Test
+    public void verifyAddTrainer() {
+
+        LoginPage loginPage = new LoginPage(driver,prop);
+        AddTrainer addTrainer = new AddTrainer(driver,prop);
+
+        //login
+        loginPage.login(prop.getProperty("username"), prop.getProperty("passw"));
+
+        //trainer management
+        addTrainer.clickOnTrainerManagement();
+        addTrainer.clickOnAddTrainer();
+        addTrainer.enterName(prop.getProperty("addtrainer.name"));
+        addTrainer.enterEmail(prop.getProperty("addtrainer.email"));
+        addTrainer.enterPhoneNumber(prop.getProperty("addtrainer.phonenumber"));
+        addTrainer.enterPassword(prop.getProperty("addtrainer.password"));
+        addTrainer.clickOnAddButton();
+        addTrainer.handleConfirmationPopup();
+
+
+    }
+
+    @Test
+    public void verifyDeleteTrainer(){
+
+        LoginPage loginPage = new LoginPage(driver,prop);
+        AddTrainer addTrainer = new AddTrainer(driver,prop);
+        loginPage.login(prop.getProperty("username"), prop.getProperty("passw"));
+        addTrainer.clickOnTrainerManagement();
+        addTrainer.deleteTrainer();
+
+    }
 }
